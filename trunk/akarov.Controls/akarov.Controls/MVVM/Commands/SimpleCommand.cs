33002a51809deed35f15471e88128fc785c522e8
for7raid <for7raid@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2012-10-26 07:14:58 +0000

    Начальная версия

---
+++ trunk/akarov.Controls/akarov.Controls/MVVM/Commands/SimpleCommand.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace akarov.Controls.MVVM.Commands
+{
+    /// <summary>
+    /// Implements the ICommand and wraps up all the verbose stuff so that you can just pass 2 delegates 1 for the CanExecute and one for the Execute
+    /// </summary>
+    public class SimpleCommand<T> : ICommand
+    {
+        /// <summary>
+        /// Gets or sets the Predicate to execute when the CanExecute of the command gets called
+        /// </summary>
+        public Predicate<T> CanExecuteDelegate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action to be called when the Execute method of the command gets called
+        /// </summary>
+        public Action<T> ExecuteDelegate { get; set; }
+
+        #region ICommand Members
+
+        public SimpleCommand(Action<T> ExecuteDelegate):this(ExecuteDelegate,null)
+        {
+
+        }
+
+        public SimpleCommand(Action<T> ExecuteDelegate, Predicate<T> CanExecuteDelegate)
+        {
+            if (ExecuteDelegate == null)
+                throw new ArgumentNullException("ExecuteDelegate");
+
+            this.ExecuteDelegate = ExecuteDelegate;
+            this.CanExecuteDelegate = CanExecuteDelegate;
+        }
+        /// <summary>
+        /// Checks if the command Execute method can run
+        /// </summary>
+        /// <param name="parameter">THe command parameter to be passed</param>
+        /// <returns>Returns true if the command can execute. By default true is returned so that if the user of SimpleCommand does not specify a CanExecuteCommand delegate the command still executes.</returns>
+        public bool CanExecute(Object parameter)
+        {
+            if (CanExecuteDelegate != null)
+                return CanExecuteDelegate((T)parameter);
+            return true;// if there is no can execute default to true
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Executes the actual command
+        /// </summary>
+        /// <param name="parameter">THe command parameter to be passed</param>
+        public void Execute(object parameter)
+        {
+            if (ExecuteDelegate != null && CanExecute(parameter))
+                ExecuteDelegate((T)parameter);
+        }
+
+        #endregion
+    }
+
+    public class SimpleCommand : ICommand
+    {
+        /// <summary>
+        /// Gets or sets the Predicate to execute when the CanExecute of the command gets called
+        /// </summary>
+        public Predicate<object> CanExecuteDelegate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the action to be called when the Execute method of the command gets called
+        /// </summary>
+        public Action ExecuteDelegate { get; set; }
+
+        #region ICommand Members
+
+        public SimpleCommand(Action ExecuteDelegate)
+            : this(ExecuteDelegate, null)
+        {
+
+        }
+
+        public SimpleCommand(Action ExecuteDelegate, Predicate<object> CanExecuteDelegate)
+        {
+            if (ExecuteDelegate == null)
+                throw new ArgumentNullException("ExecuteDelegate");
+
+            this.ExecuteDelegate = ExecuteDelegate;
+            this.CanExecuteDelegate = CanExecuteDelegate;
+        }
+        /// <summary>
+        /// Checks if the command Execute method can run
+        /// </summary>
+        /// <param name="parameter">THe command parameter to be passed</param>
+        /// <returns>Returns true if the command can execute. By default true is returned so that if the user of SimpleCommand does not specify a CanExecuteCommand delegate the command still executes.</returns>
+        public bool CanExecute(Object parameter)
+        {
+            if (CanExecuteDelegate != null)
+                return CanExecuteDelegate(null);
+            return true;// if there is no can execute default to true
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Executes the actual command
+        /// </summary>
+        /// <param name="parameter">THe command parameter to be passed</param>
+        public void Execute(object parameter)
+        {
+            if (ExecuteDelegate != null && CanExecute(parameter))
+                ExecuteDelegate();
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/akarov.Controls/akarov.Controls/Utils/AssemblyAttributes.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace akarov.Controls.Utils
+{
+    public static class AssemblyAttributes
+    {
+        private  const string propertyNameTitle = "Title";
+        private  const string propertyNameDescription = "Description";
+        private  const string propertyNameProduct = "Product";
+        private  const string propertyNameCopyright = "Copyright";
+        private  const string propertyNameCompany = "Company";
+
+      
+        /// Gets the title property, which is display in the About dialogs window title.
+        /// </summary>
+        public static string ProductTitle
+        {
+            get
+            {
+                string result = CalculatePropertyValue<AssemblyTitleAttribute>(propertyNameTitle);
+                if (string.IsNullOrEmpty(result))
+                {
+                    // otherwise, just get the name of the assembly itself.
+                    result = Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Gets the application's version information to show.
+        /// </summary>
+        public static string Version
+        {
+            get
+            {
+                string result = string.Empty;
+                // first, try to get the version string from the assembly.
+                Version version = Assembly.GetEntryAssembly().GetName().Version;
+                if (version != null)
+                {
+                    result = version.ToString();
+                }
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Gets the description about the application.
+        /// </summary>
+        public static string Description
+        {
+            get { return CalculatePropertyValue<AssemblyDescriptionAttribute>(propertyNameDescription); }
+        }
+
+        /// <summary>
+        ///  Gets the product's full name.
+        /// </summary>
+        public static string Product
+        {
+            get { return CalculatePropertyValue<AssemblyProductAttribute>(propertyNameProduct); }
+        }
+
+        /// <summary>
+        /// Gets the copyright information for the product.
+        /// </summary>
+        public static string Copyright
+        {
+            get { return CalculatePropertyValue<AssemblyCopyrightAttribute>(propertyNameCopyright); }
+        }
+
+        /// <summary>
+        /// Gets the product's company name.
+        /// </summary>
+        public static string Company
+        {
+            get { return CalculatePropertyValue<AssemblyCompanyAttribute>(propertyNameCompany); }
+        }
+
+       
+        /// <summary>
+        /// Gets the specified property value either from a specific attribute, or from a resource dictionary.
+        /// </summary>
+        /// <typeparam name="T">Attribute type that we're trying to retrieve.</typeparam>
+        /// <param name="propertyName">Property name to use on the attribute.</param>
+        /// <param name="xpathQuery">XPath to the element in the XML data resource.</param>
+        /// <returns>The resulting string to use for a property.
+        /// Returns null if no data could be retrieved.</returns>
+        private static string CalculatePropertyValue<T>(string propertyName)
+        {
+            string result = string.Empty;
+            // first, try to get the property value from an attribute.
+            object[] attributes = Assembly.GetEntryAssembly().GetCustomAttributes(typeof(T), false);
+            if (attributes.Length > 0)
+            {
+                T attrib = (T)attributes[0];
+                PropertyInfo property = attrib.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+                if (property != null)
+                {
+                    result = property.GetValue(attributes[0], null) as string;
+                }
+            }
+            return result;
+        }
+    }
+}
