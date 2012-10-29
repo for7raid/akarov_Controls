d986a6ec35c0b6dc4e3bcaf78bb89136908eee95
for7raid <for7raid@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2012-10-29 18:30:03 +0000

    Доделал SingleInstance Сделал утилиту для ассоциирования расширения файла с текущей программой

---
+++ akarov.Controls/akarov.Controls/Utils/FileAssotiation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Windows;
+using System.Reflection;
+using System.IO;
+
+namespace akarov.Controls.Utils
+{
+    public class FileAssotiation
+    {
+        public static void AssotiateExtention(string ext)
+        {
+            try
+            {
+                var assembly = Assembly.GetEntryAssembly();
+                var appName = Path.GetFileNameWithoutExtension(assembly.Location).Replace(" ", "");
+
+
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey(ext, true) ?? Registry.ClassesRoot.CreateSubKey(ext);
+                key.SetValue("", ext.Replace(".", "") + "_auto_file");
+                key.Close();
+
+                key = Registry.ClassesRoot.OpenSubKey(ext.Replace(".", "") + "_auto_file", true) ??
+                        Registry.ClassesRoot.CreateSubKey(ext.Replace(".", "") + "_auto_file");
+                key.SetValue("", "");
+
+                var iconKey = key.OpenSubKey("DefaultIcon",true) ?? key.CreateSubKey("DefaultIcon");
+                iconKey.SetValue("", assembly.Location);
+
+                key = key.OpenSubKey("Shell\\Open\\command", true) ??
+                        key.CreateSubKey("Shell\\Open\\command");
+
+                key.SetValue("", "\"" + assembly.Location + "\" \"%1\"");
+                key.Close();
+
+               
+                key.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw new InvalidOperationException("Не могу зарегистрировать расширение файла " + ext + 
+                                                    "\r\nПрограмма должна быть запущена с правами администратора", 
+                                                    ex);
+            }
+
+        }
+    }
+}
