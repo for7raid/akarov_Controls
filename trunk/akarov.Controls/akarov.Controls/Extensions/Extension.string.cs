79bfa868ad4e7fb66b34633a9a50ecbb420b999e
for7raid <for7raid@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2012-10-31 18:24:33 +0000

    Класс расширений для текста

---
+++ trunk/akarov.Controls/akarov.Controls/Extensions/Extension.string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace akarov.Controls.Extensions
+{
+    public static partial class Extension
+    {
+        public static string F(this string input,params object[] args)
+        {
+            return String.Format(input, args);
+        }
+    }
+}
